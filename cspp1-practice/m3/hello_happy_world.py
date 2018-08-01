01f68c2e7d55844dc01bb5ce79e58deafbd5ec5d
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-01 18:59:47 +0530

    second program

---
+++ cspp1-practice/m3/hello_happy_world.py
@@ -0,0 +1,6 @@
+"""
+Storing the integer type value in happy
+"""
+happy = int(input("enter the value"))
+if happy > 2:
+    print("Hello world")
--- cspp1-practice/m3/helloworld.py
+++ cspp1-practice/m3/helloworld.py
@@ -1,1 +1,4 @@
-print('hello world')
+"""
+Basic
+"""
+print("Hello world")
