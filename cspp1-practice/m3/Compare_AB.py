70de17be862ca41d156c78ca091287ada6633f7d
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-02 12:57:10 +0530

    " -> 0 of 1 passed. pylint: 6.00/10 "

---
+++ cspp1-practice/m3/Compare_AB.py
@@ -0,0 +1,20 @@
+m=input("Enter the value of a")
+n=input("Enter the value of b")
+
+try:
+    Var A =int(a)
+    Var B =(b)
+    if Var A< Var B:
+        print("smaller")
+    elif Var A == Var B:
+        print("equal")
+    else:
+        print("bigger")
+
+except:
+    print("string involved")
+    
+    
+    
+    
+    
