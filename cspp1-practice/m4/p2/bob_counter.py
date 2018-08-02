34898790a3e43f684a11a8fc0696a4d0bb1a5a90
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-02 17:54:52 +0530

    " -> 5 of 5 passed. pylint: -5.83/10 "

---
+++ cspp1-practice/m4/p2/bob_counter.py
@@ -0,0 +1,13 @@
+s =input()
+a = 0
+b = 1
+c = 2
+count = 0
+while(c<len(s)):
+      if s[a]=='b'and s[b]=='o'and s[c]=='b':
+          count += 1
+      a += 1
+      b += 1
+      c += 1
+print(count)   
+
