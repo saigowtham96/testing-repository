7752c1641d5c4e25e546463fddf7bc720a3809b4
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-04 17:27:22 +0530

    " -> 3 of 3 passed. pylint: 9.09/10 "

---
+++ cspp1-practice/m5/p3/square_root_bisection.py
@@ -0,0 +1,13 @@
+"""Bisectionmethod"""
+A = int(input())
+EP = 0.01
+L = 0.0
+H = A
+ANS = (H + L)/2.0
+while abs(ANS**2 - A) >=EP:
+    if ANS**2 < A:
+        L = ANS
+    else:
+        H = ANS
+    ANS = (H + L)/2.0
+print(ANS)
