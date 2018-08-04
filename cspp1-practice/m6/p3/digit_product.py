591dd1cfc780328e4fdaafe01b4eff2bd1b32043
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-04 10:55:58 +0530

    " -> 2 of 7 passed. pylint: 0.00/10 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -1,10 +1,8 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
-	input: 123
-	output: 6
+
 '''
-NUM = int(input("enter the number"))
+NUM = int(input())
 I = 1
 while I<NUM:
    A = NUM%10
