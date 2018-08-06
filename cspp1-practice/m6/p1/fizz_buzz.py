1f0a7e6d2d6393b1d7714247ba5c2579325be364
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-06 17:39:10 +0530

    " -> 0 of 7 passed. pylint: -0.83/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -4,24 +4,21 @@ For each multiple of 3, print "Fizz" instead of the number.
 For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 '''
-def main():
-    '''
-    Read number from the input, store it in variable num.
-    '''
 n = int(input())
-i=0
-for i in range(1,n,1):
-    if(n%3==0 and n%5==0):
-       print("fizz")
-       print("buzz")
-       break
-    elif(n%3==0):
+# n = 16
+i=1
+for i in range(1,n+1,1):
+    if(i%3==0 and i%5==0):
         print("fizz")
-        break
-    elif(n%5==0):
+        print("buzz")
+       # break
+    elif(i%3==0):
+        print("fizz")
+        # break
+    elif(i%5==0):
         print("Buzz")
-        break
+        # break
     else:
         print(i)
-                 
+    i=i+1          
      
