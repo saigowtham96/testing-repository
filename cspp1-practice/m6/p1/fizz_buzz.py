1d651d72a046c569a9037c88277e3c7313680c3b
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-06 17:41:38 +0530

    " -> 7 of 7 passed. pylint: -0.91/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -5,7 +5,6 @@ For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 '''
 n = int(input())
-# n = 16
 i=1
 for i in range(1,n+1,1):
     if(i%3==0 and i%5==0):
@@ -20,5 +19,5 @@ for i in range(1,n+1,1):
         # break
     else:
         print(i)
-    i=i+1          
+        # i=i+1          
      
