a8c03ae1cc94b78b9335c82e12599d45fe0f355a
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-07 20:32:25 +0530

    " -> 1 of 10 passed. pylint: -7.78/10 "

--- cspp1-practice/m8/p2/assignment2.py
+++ cspp1-practice/m8/p2/assignment2.py
@@ -14,7 +14,7 @@ def sum_of_digits(num_var):
     if num_var == 0:
        return 0
     else:   
-       return int(n % 10 + sum_of_digits(num_var//10))
+       return int(n % 10 + sum_of_digits(num_var // 10))
 
 
 
