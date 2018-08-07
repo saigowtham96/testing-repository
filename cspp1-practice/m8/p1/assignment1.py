1dec83793851799ac06d21b35fe7b0c45296276e
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-07 20:00:40 +0530

    " -> 10 of 10 passed. pylint: 1.11/10 "

--- cspp1-practice/m8/p1/assignment1.py
+++ cspp1-practice/m8/p1/assignment1.py
@@ -4,16 +4,16 @@
 # This function takes in one number and returns one number.
 
 
-def factorial(N):
+def factorial(N_1):
     '''
     n is positive Integer
 
     returns: a positive integer, the factorial of n.
     '''
     # Your code here
-    if N <= 1:
+    if N_1 <= 1:
         return 1
-    return N*factorial(N-1)
+    return N_1*factorial(N_1-1)
     
 
 
