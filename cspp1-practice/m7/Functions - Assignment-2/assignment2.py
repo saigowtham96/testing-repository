529f36a1e33ead15dfe9686237c7238e61b39e43
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-07 22:33:49 +0530

    " -> 10 of 10 passed. pylint: 9.50/10 "

--- cspp1-practice/m7/Functions - Assignment-2/assignment2.py
+++ cspp1-practice/m7/Functions - Assignment-2/assignment2.py
@@ -8,7 +8,7 @@ def paying_debtoffinayear(bal_ance, annual_interestrate):
         ba_la = bal_ance
         while i != 0:
             upd_bal = ba_la - p_a_y
-            ba_la = upd_bal + ((annual_interestrate / 12.0) * upd_bal)
+            ba_la = upd_bal + ((annual_interestrate/12.0) * upd_bal)
             i -= 1
         if ba_la > 0:
             p_a_y += 10
