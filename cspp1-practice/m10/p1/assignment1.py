1117d2013bf04bc0618a0109d32d212f3629d753
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-09 20:51:13 +0530

    " -> 10 of 10 passed. pylint: 9.41/10 "

--- cspp1-practice/m10/p1/assignment1.py
+++ cspp1-practice/m10/p1/assignment1.py
@@ -6,7 +6,7 @@ that is comprised of lowercase English letters - all lowercase English letters
 that are not in letters_guessed
 '''
 import string
-alph = string.ascii_lowercase
+Alph_bet = string.ascii_lowercase
 
 def get_available_letters(letters_guessed):
     '''
@@ -15,7 +15,7 @@ def get_available_letters(letters_guessed):
       yet been guessed.
     '''
     remain = []
-    for i in alph:
+    for i in Alph_bet:
         if i not in letters_guessed:
             remain.append(i)
     return ''.join(remain)
