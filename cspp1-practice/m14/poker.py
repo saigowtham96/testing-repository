d7a98ca783500312f030560cd3b9694e8e52dea5
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-15 22:33:28 +0530

    " -> 8 of 8 passed. pylint: 7.23/10 "

--- cspp1-practice/m14/poker.py
+++ cspp1-practice/m14/poker.py
@@ -3,6 +3,51 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
+def dict_fun(hand):
+    d = {}
+    for i in range(len(hand)):
+        if hand[i][0] in d:
+            d[hand[i][0]] +=1
+        else:
+            d[hand[i][0]] = 1
+    return d
+def is_fourofakind(hand):
+    flag = 0
+    dict_four = {}
+    dict_four = dict_fun(hand)
+    if len(dict_four) == 2:
+        if 1 in dict_four.values() and 4 in dict_four.values():
+            flag = 1
+    return flag
+def is_fullhouse(hand):
+    flag = 0
+    dict_full = {}
+    dict_full = dict_fun(hand)
+    if len(dict_full) == 2:
+        if 2 in dict_full.values() and 3 in dict_full.values():
+            flag = 1
+    return flag
+def is_threeofakind(hand):
+    flag = 0
+    dict_three = {}
+    dict_three = dict_fun(hand)
+    if len(dict_three) == 3 and 3 in dict_three.values():
+        flag = 1
+    return flag
+def is_twopair(hand):
+    flag = 0
+    dict_twopair = {}
+    dict_twopair = dict_fun(hand)
+    if len(dict_twopair) == 3 and 2 in dict_twopair.values():
+        flag = 1
+    return flag
+def is_onepair(hand):
+    flag = 0
+    dict_onepair = {}
+    dict_onepair = dict_fun(hand)
+    if len(dict_onepair) == 4 and 2 in dict_onepair.values():
+        flag = 1
+    return flag
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -13,24 +58,20 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    list_l = []
-    #list_b = []
-    dict_d = {'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+    
+    my_dict = {'T':10,'J':11,'Q':12,'K':13,'A':14}
+    l = []
     for i in range(len(hand)):
-        if hand[i][0] not in dict_d:
-            list_l.append(int(hand[i][0]))
+        if hand[i][0] in my_dict:
+            l.append(int(my_dict[hand[i][0]]))
         else:
-            list_l.append(dict_d[hand[i][0]])
-    #print(list)
-    list_l.sort()
-    #print(list)
-    v_c = 0
-    for i in range((len(list_l))-1):
-        if list_l[i+1]-list_l[i] == 1:
-            v_c = v_c + 1
-    if v_c == len(list_l)-1:
-        return True
-    return False
+            l.append(int(hand[i][0]))
+    minimum = min(l)
+    for i in range(len(l)):
+        if minimum not in l:
+            return 0
+        minimum += 1
+    return 1
 def is_flush(hand):
     '''
         How do we find out if the given hand is a flush?
@@ -40,16 +81,17 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
-    last = []
-    for i in range(len(hand)):
-        last.append(hand[i][1])
-    v_e = 0
-    for each_val in range(len(last)-1):
-        if last[each_val] == last[each_val+1]:
-            v_e = v_e + 1
-    if v_e == (len(last)-1):
-        return True
-    return False
+    face_val=[]
+    count=0
+    for i in range(len(hand)-1):
+        if hand[i][1]==hand[i+1][1]:
+            count=count+1
+    if count==len(hand)-1:
+        return 1
+    else:
+        return 0
+
+
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
@@ -58,6 +100,7 @@ def hand_rank(hand):
         The first version should identify if the given hand is a straight
         or a flush or a straight flush.
     '''
+
     # By now you should have seen the way a card is represented.
     # If you haven't then go the main or poker function and print the hands
     # Each card is coded as a 2 character string. Example Kind of Hearts is KH
@@ -66,19 +109,31 @@ def hand_rank(hand):
     # What would be the logic to determine if a hand is a straight or flush?
     # Let's not think about the logic in the hand_rank function
     # Instead break it down into two sub functions is_straight and is_flush
+
     # check for straight, flush and straight flush
     # best hand of these 3 would be a straight flush with the return value 3
     # the second best would be a flush with the return value 2
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
-    # max in poker(= function uses these return values to select the best hand
+    # max in poker function uses these return values to select the best hand
     if is_straight(hand) and is_flush(hand):
-       return 3
+        return 9
+    if is_fourofakind(hand):
+        return 8
+    if is_fullhouse(hand):
+        return 7
     if is_flush(hand):
-       return 2
+        return 6
     if is_straight(hand):
-       return 1
-    return 0
+        return 5
+    if is_threeofakind(hand):
+        return 4
+    if is_twopair(hand):
+        return 3
+    if is_onepair(hand):
+        return 2
+    return 1
+
 def poker(hands):
     '''
         This function is completed for you. Read it to learn the code.
@@ -89,6 +144,7 @@ def poker(hands):
 
         Output: Return the winning poker hand
     '''
+
     # the line below may be new to you
     # max function is provided by python library
     # learn how it works, in particular the key argument, from the link
@@ -96,7 +152,9 @@ def poker(hands):
     # hand_rank is a function passed to max
     # hand_rank takes a hand and returns its rank
     # max uses the rank returned by hand_rank and returns the best hand
+    #return max(hands,key=hand_rank)
     return max(hands, key=hand_rank)
+
 if __name__ == "__main__":
     # read the number of test cases
     COUNT = int(input())
@@ -108,4 +166,9 @@ if __name__ == "__main__":
         HANDS.append(ha)
     # test the poker function to see how it works
     print(' '.join(poker(HANDS)))
+    #print(is_straight(['8D', '9D', 'TS', 'JC']))
+    #print(is_fourofakind(['AD', 'AH', 'AC', '2S', 'TD']))
+
+            
+
     
