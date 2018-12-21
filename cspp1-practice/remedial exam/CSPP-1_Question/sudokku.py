317940354bc083e3ce2c9fb8c7b8eb4d9ad485a3
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-21 11:14:55 +0530

    "cspp1_m10_a1_1: 10/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 2/10 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudokku.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudokku.py
@@ -1,75 +1,41 @@
-import re
-def main():
-	inp = input()
-	inp = list(inp)
-	list1 = []
-	i = 0
-	temp = []
-	for i in range(len(inp)):
-		if i%9==0 and i!=0:
-			list1.append(temp)		
-			temp = []
-		temp.append(inp[i])
-	list1.append(temp)
-	for i in range(len(list1)):
-		for j in range(len(list1[0])):
-			if list1[i][j] == '.':
-				print(sudoku(list1,i,j))
-def sudoku(list1,i,j):
-	# for i in range(len(lis)):
-	# 	print(list1[i])
-	# 	print()
-	temp1 = []
-	for k in range(len(list1)):
-		tem.append(list1[k][j])
-	for m in range(len(list1[0])):
-		tem.append(list1[i][m])
-	if (i >= 0 and i <= 2) and (j >= 0 and j <= 2):
-		for x in range(0,3):
-			for y in range(0,3):
-				temp1.append(list1[x][y])
-	elif (i >= 0 and i <= 2) and (j >= 3 and j <= 5):
-		for x in range(0,3):
-			for y in range(3,6):
-				temp1.append(list1[x][y])
-	elif (i >= 0 and i <= 2) and (j >= 6 and j <= 8):
-		for x in range(0,3):
-			for y in range(6,9):
-				temp1.append(list1[x][y])
-	elif (i >= 3 and i <= 5) and (j >= 0 and j <= 2):
-		for x in range(3,6):
-			for y in range(0,3):
-				temp1.append(list1[x][y])
-	elif (i >= 3 and i <= 5) and (j >= 3 and j <= 5):
-		for x in range(3,6):
-			for y in range(3,6):
-				temp1.append(list1[x][y])
-	elif (i >= 3 and i <= 5) and (j >= 6 and j <= 8):
-		for x in range(3,6):
-			for y in range(6,9):
-				temp1.append(list1[x][y])
-	elif (i >= 6 and i <= 8) and (j >= 0 and j <= 2):
-		for x in range(6,9):
-			for y in range(0,3):
-				temp1.append(list1[x][y])
-	elif (i >= 6 and i <= 8) and (j >= 3 and j <= 5):
-		for x in range(6,9):
-			for y in range(3,6):
-				temp1.append(list1[x][y])
-	elif (i >= 6 and i <= 8) and (j >= 6 and j <= 8):
-		for x in range(6,9):
-			for y in range(6,9):
-				temp1.append(list1[x][y])
-	string1 = ''.join(tem)
-	string1 = string.replace(".", "")
-	string = list(string)
-	inte = list(map(int,string1))
-	whole = [1,2,3,4,5,6,7,8,9]
-	numbers = []
-	for z in range(len(whole)):
-		if whole[z] not in inte:
-			numbers.append(whole[z])
-	string2 = list(map(str,numbers))
-	string2 = ''.join(string2)
-	return string2
-main()
+def print_grid(arr): 
+    for i in range(9): 
+        print(arr[i]) 
+def create_set(g, row, col):
+    lis= set()
+    for i in range(9):
+        if g[row][i] != '0':
+            lis.add(g[row][i])
+        if g[i][col] != '0':
+            lis.add(g[i][col])
+    return lis
+
+def possibilities(g):
+    for i in range(9):
+        for j in range(9):
+            res = ""
+            s = set()
+            if g[i][j] == '0':
+                s = create_set(g, i, j)
+                # print(s)
+            if len(s) != 0:
+                for each in "123456789":
+                    if each not in s:
+                        res += each
+                print(res)
+
+if __name__=="__main__": 
+      
+    # creating a 2D array for the grid 
+    grid=[['0' for x in range(9)]for y in range(9)] 
+    # print_grid(grid)
+      
+    given_input = input()
+    k = 0
+    for i in range(9):
+        for j in range(9):
+            if given_input[k] != '.':
+                grid[i][j] = given_input[k]
+            k += 1
+    # print_grid(grid)
+    possibilities(grid)
