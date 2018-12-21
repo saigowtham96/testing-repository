75afed6ca8aaaac3f1ac672ea603cd070c287e75
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-21 10:25:37 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -16/10 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudokku.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudokku.py
@@ -1,32 +1,12 @@
-
-def check_sudoku(sudoku):
-    '''
-        Your solution goes here. You may add other helper functions as needed.
-        The function has to return True for a valid sudoku grid and false otherwise
-    '''
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    if row_check(sudoku, list1) and column_check(sudoku, list1):
-        return True
-    return False
-
-
-
-
-
-
-
-def main():
-    '''
-        main function to read input sudoku from console
-        call check_sudoku function and print the result to console
-    '''
-    # initialize empty list
-    sudoku = [] 
-    # read a line and append row to list
-    row = list(map(int, input()))
-    sudoku.append(row)
-    # call solution function and print result to console
-    print(sudoku)
-
-if __name__ == '__main__':
-    main()
+def solveSudoku(grid, i=0, j=0):
+            i,j = findNextCellToFill(grid, i, j)
+            if i == -1:
+                    return True
+            for e in range(1,10):
+                    if isValid(grid,i,j,e):
+                            grid[i][j] = e
+                            if solveSudoku(grid, i, j):
+                                    return True
+                            # Undo the current cell for backtracking
+                            grid[i][j] = 0
+            return False
