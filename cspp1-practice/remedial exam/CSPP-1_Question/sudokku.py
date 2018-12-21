cc6beb7358420d586f409f19f6f1ab037bb89958
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-21 12:17:32 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudokku.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudokku.py
@@ -39,5 +39,7 @@ if __name__=="__main__":
             if given_input[k] != '.':
                 grid[i][j] = given_input[k]
             k += 1
+            if given_input > 81
+            print("Invalid input")
     # print_grid(grid)
     possibilities(grid)
