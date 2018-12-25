6e0596acda771b7849f85b53d8d2c4642b72700d
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-25 11:27:50 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -21/10 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudoku.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudoku.py
@@ -10,7 +10,7 @@ In this method :
 """
 def validateSudoku(sudoku):
         if len(given_input) > 81 or len(given_input) < 81:
-                return "Invalid input"
+                print("Invalid input")
         
         
         
