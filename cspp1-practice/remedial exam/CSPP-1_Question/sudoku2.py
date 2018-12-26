e73fabcbde55200e8055270e9a7391878bed0eba
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-26 11:08:04 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudoku2.py
@@ -0,0 +1,62 @@
+"""
+In this method :
+ * Check there are only 81 values
+ * iterate through each row in the sudoku and if you find any duplicate values
+ 	raise an exception
+ * iterate through each column in the sudoku and if you find any duplicate values
+	raise an exception
+ * iterate through each subgrid(3x3) in the sudoku and if you find any duplicate values
+	raise an exception
+"""
+def validInput(info):
+    if len(info) != 81:
+        raise Exception("Invalid input")
+    elif '.' not in info:
+        raise Exception("Given sudoku is solved")
+def validateSudoku(sudoku):
+	pass
+"""
+This  method should retunn all the values present in the ith row
+"""
+def getRowValues():
+	pass
+"""
+This  method should retunn all the values present in the ith column
+"""
+def getColumnValues():
+	pass
+
+"""
+This  method should retunn all the values present in the i,j th subgrid
+"""
+def getGridValues():
+	pass
+"""
+This method should collect all the available values present for a "."
+You should get the values present in row,column,grid.
+Then you should return the values that doesnot exist in the previous values.
+"""
+def possibleValues():
+	pass
+"""
+Read the input and store the values in an appropriate data sturcture.
+Then travese through each value, if you get a "." then collect the possible values
+"""
+def main():
+	info1 = input()
+        info = list(info1)
+        j = 0
+        try:
+                while(i <81):
+                        row = []
+                        for k in range(0,9):
+                                row.append(info[j])
+                                j += 1
+                        sudoku.append(row)
+                        validInput(data1)
+        validSudoku(sudoku)
+        possibilites(sudoku)
+    except Exception as e:
+        print(e)
+
+
