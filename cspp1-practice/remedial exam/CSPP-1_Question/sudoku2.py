056539a30b537bc84aad04e74c128a3acbb30a83
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-26 12:14:07 +0530

    "cspp1_m10_a1_1: 3/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/10 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudoku2.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudoku2.py
@@ -2,11 +2,11 @@
 In this method :
  * Check there are only 81 values
  * iterate through each row in the sudoku and if you find any duplicate values
- 	raise an exception
+    raise an exception
  * iterate through each column in the sudoku and if you find any duplicate values
-	raise an exception
+    raise an exception
  * iterate through each subgrid(3x3) in the sudoku and if you find any duplicate values
-	raise an exception
+    raise an exception
 """
 def validInput(info):
     if len(info) != 81:
@@ -14,48 +14,63 @@ def validInput(info):
     elif '.' not in info:
         raise Exception("Given sudoku is solved")
 def validateSudoku(sudoku):
-	pass
+    for x in range(0,9):
+        var_A = getRowValues(x, sudoku)
+        if(len(set(var_A))) != len(var_A):
+            raise Exception("duplicates are present")
+        columnvar = getColumnValues(x, sudoku)
+        if(len(set(columnvar))) != len(columnvar):
+            raise Exception("duplicates are present")
+    
 """
 This  method should retunn all the values present in the ith row
 """
-def getRowValues():
-	pass
+def getRowValues(cell, sudoku):
+    row = []
+    for x in range(cell):
+        if x != '.':
+            row.append(x)
+    return row 
 """
 This  method should retunn all the values present in the ith column
 """
-def getColumnValues():
-	pass
+def getColumnValues(cell, sudoku):
+    col = []
+    for y in range(cell):
+        if row[cell] != '.':
+            col.append(row[cell])
+    return col
 
 """
 This  method should retunn all the values present in the i,j th subgrid
 """
 def getGridValues():
-	pass
+    pass
 """
 This method should collect all the available values present for a "."
 You should get the values present in row,column,grid.
 Then you should return the values that doesnot exist in the previous values.
 """
 def possibleValues():
-	pass
+    pass
 """
 Read the input and store the values in an appropriate data sturcture.
 Then travese through each value, if you get a "." then collect the possible values
 """
 def main():
-	info1 = input()
-	info = list(info1)
-	j = 0
-	try:
-		validInput(info)
-		while(j<81):
-			row = []
-			for k in range(0,9):
-				row.append(info)
-				j += 1
-			sudoku.append(row)
+    info1 = input()
+    info = list(info1)
+    j = 0
+    try:
+        validInput(info)
+        while(j<81):
+            row = []
+            for k in range(0,9):
+                row.append(info)
+                j += 1
+            sudoku.append(row)
         validateSudoku(sudoku)
     except Exception as e:
-    	print(e)
+        print(e)
 if __name__ == '__main__':
     main()
