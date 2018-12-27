a546d74a1ad414455697fbef896d1b6fbd2d4a34
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-27 08:40:39 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudoku.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudoku.py
@@ -16,11 +16,11 @@ def validateInput(data):
         raise Exception("Given sudoku is solved")
 def validateSudoku(sudoku):
     for x in range(9):
-        row_variable=getRowValues(x,sudoku)
-        if len(row_variable) != len(set(row_variable)):
+        row_var=getRowValues(x,sudoku)
+        if len(set(row_var)) != len(row_var):
             raise Exception("Invalid Sudoku:Duplicate values")
-        column_variable=getColumnValues(x,sudoku)
-        if len(column_variable) != len(set(column_variable)):
+        column_var=getColumnValues(x,sudoku)
+        if len(set(column_var)) != len(column_var):
             raise Exception("Invalid Sudoku:Duplicate values")
         
 
@@ -46,47 +46,7 @@ def getColumnValues(cell, sudoku):
 This  method should retunn all the values present in the i,j th subgrid
 """
 def getGridValues(i,j,sudoku):
-    grid=[]
-    if (i<=0 and i>3) and (j<=0 and j>3):
-        for sub_row in range(0,3):
-            for sub_col in range(0,3):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=0 and i>3) and (j<=3 and j>6):
-        for sub_row in range(0,3):
-            for sub_col in range(3,6):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=0 and i>3) and (j<=6 and j>9):
-        for sub_row in range(0,3):
-            for sub_col in range(6,9):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=3 and i>6) and (j<=0 and j>3):
-        for sub_row in range(3,6):
-            for sub_col in range(0,3):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=3 and i>6) and (j<=3 and j>6):
-        for sub_row in range(3,6):
-            for sub_col in range(3,6):
-                grid.append(sudoku[sub_row][sub_col])
-
-    if (i<=3 and i>6) and (j<=6 and j>9):
-        for sub_row in range(3,6):
-            for sub_col in range(6,9):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=0 and i>3) and (j<=0 and j>3):
-        for sub_row in range(6,9):
-            for sub_col in range(0,3):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=0 and i>3) and (j<=3 and j>6):
-        for sub_row in range(6,9):
-            for sub_col in range(3,6):
-                grid.append(sudoku[sub_row][sub_col])
-    if (i<=0 and i>3) and (j<=6 and j>9):
-        for sub_row in range(6,9):
-            for sub_col in range(6,9):
-                grid.append(sudoku[sub_row][sub_col])
-    # print(grid)
-    return grid
-                
+        
 """
 This method should collect all the available values present for a "."
 You should get the values present in row,column,grid.
@@ -96,9 +56,9 @@ def possibleValues(sudoku):
     for x in range(len(sudoku)):
         for y in range(len(sudoku[0])):
             if sudoku[x][y]=='.':
-                row_variable=getRowValues(x,sudoku)
-                column_variable=getColumnValues(y,sudoku)
-                result=row_variable+column_variable
+                row_var=getRowValues(x,sudoku)
+                column_var=getColumnValues(y,sudoku)
+                result=row_var+column_var
                 elements=''
                 for i in range(1,10):
                     if str(i) not in result:
@@ -110,17 +70,17 @@ Read the input and store the values in an appropriate data sturcture.
 Then travese through each value, if you get a "." then collect the possible values
 """
 def main():
-    input_data= input()
+    data1= input()
     data=list(input_data)
-    i=0
+    j=0
     sudoku=[]
     try:
         validateInput(input_data)
-        while (i < 81):
+        while (j < 81):
             row=[]
-            for j in range(9):
-                row.append(data[i])
-                i=i+1
+            for k in range(9):
+                row.append(data[j])
+                j=j+1
             sudoku.append(row)
         validateSudoku(sudoku)
         possibleValues(sudoku)
