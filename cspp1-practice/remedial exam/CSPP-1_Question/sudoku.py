b0d11d5bf5c749e3073e701e51a57a08757294af
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-27 10:36:50 +0530

    "cspp1_m10_a1_1: 5/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- cspp1-practice/remedial exam/CSPP-1_Question/sudoku.py
+++ cspp1-practice/remedial exam/CSPP-1_Question/sudoku.py
@@ -101,7 +101,8 @@ def possibleValues(sudoku):
             if sudoku[x][y]=='.':
                 row_var=getRowValues(x,sudoku)
                 column_var=getColumnValues(y,sudoku)
-                result=row_var+column_var
+                grid = getGridValues(j,k,sudoku)
+                result=row_var+column_var+grid
                 elements=''
                 for i in range(1,10):
                     if str(i) not in result:
@@ -127,7 +128,7 @@ def main():
             sudoku.append(row)
         validateSudoku(sudoku)
         possibleValues(sudoku)
-        getGridValues(j,k,sudoku)
+        
     except Exception as e:
         print(e)
     # print(sudoku)
