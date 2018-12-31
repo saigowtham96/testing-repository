6f1326eb8b8a2a3f43a1b08f330601facc43dc56
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-31 10:46:10 +0530

    "cspp1_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 7/10 "

--- cspp1-practice/cspp1 week1 exam/CSPP-1Week1Exam/week1exam.py
+++ cspp1-practice/cspp1 week1 exam/CSPP-1Week1Exam/week1exam.py
@@ -16,34 +16,13 @@ def getimage(word):
         print(result)
     print(count)
 
-# def getdisplay(word):
-#     lines = word.split(":")
-#     starttag = "backgroundcolor\""
-#     endtag = "\""
-#     list2 = []
-#     counter = 0
-#     result1 = 0
-#     for j in lines:
-#         list2.append(j)
-#         for temp1 in list2:
-#             if starttag in temp1:
-#                 first = temp1.index(starttag)
-#                 temp1 = temp1[start+len(starttag)]
-#                 last = temp1.index(endtag)
-#                 result1 = temp[:end]
-#             counter = counter+1
-#             print(result1)
-#         print(counter)
-
-
-
         
 def main():
     page = open("file.html", errors = "ignore")
     print(page)
     for word in page:
         #print(word)
-        getimage(word)
+        getimage(page)
         # getdisplay(word)
 
 if __name__ == '__main__':
