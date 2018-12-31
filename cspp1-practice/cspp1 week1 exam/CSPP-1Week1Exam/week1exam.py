de46a9722cc5dafea42cbe73d0f9dc40727987e2
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-12-31 10:44:40 +0530

    "cspp1_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 7/10 "

--- cspp1-practice/cspp1 week1 exam/CSPP-1Week1Exam/week1exam.py
+++ cspp1-practice/cspp1 week1 exam/CSPP-1Week1Exam/week1exam.py
@@ -4,7 +4,6 @@ def getimage(word):
     lasttag = "\""
     list1 = []
     count = 0
-    result = 0
     for i in line:
         list1.append(i)
         for temp in list1:
@@ -13,28 +12,28 @@ def getimage(word):
                 temp = temp[start+len(firsttag):]
                 end = temp.index(lasttag)
                 result = temp[:end]
-                count = count+1
-            print(result)
-        print(count)
+        count = count+1
+        print(result)
+    print(count)
 
-def getdisplay(word):
-    lines = word.split("back")
-    starttag = "groundcolor\""
-    endtag = "\""
-    list2 = []
-    counter = 0
-    result1 = 0
-    for j in lines:
-        list2.append(j)
-        for temp1 in list2:
-            if starttag in temp1:
-                first = temp1.index(starttag)
-                temp1 = temp1[start+len(starttag)]
-                last = temp1.index(endtag)
-                result1 = temp[:end]
-            counter = counter+1
-            print(result1)
-        print(counter)
+# def getdisplay(word):
+#     lines = word.split(":")
+#     starttag = "backgroundcolor\""
+#     endtag = "\""
+#     list2 = []
+#     counter = 0
+#     result1 = 0
+#     for j in lines:
+#         list2.append(j)
+#         for temp1 in list2:
+#             if starttag in temp1:
+#                 first = temp1.index(starttag)
+#                 temp1 = temp1[start+len(starttag)]
+#                 last = temp1.index(endtag)
+#                 result1 = temp[:end]
+#             counter = counter+1
+#             print(result1)
+#         print(counter)
 
 
 
@@ -45,7 +44,7 @@ def main():
     for word in page:
         #print(word)
         getimage(word)
-        getdisplay(word)
+        # getdisplay(word)
 
 if __name__ == '__main__':
     main()
